e31fddaab9e470f258ebe96521ecf591bff6b126
Hector <hector@lecuanda.com>
2018-08-21 22:15:10 +0000

    adj fw creation

--- Firewall.mk
+++ Firewall.mk
@@ -8,7 +8,6 @@ IN     = --direction=INGRESS
 IN    += --source-ranges=$(MYIP)/32
 IN    += --target-tags=bastion
 OUT    = --direction=EGRESS
-OUT   += --source-tags=bastion
 OUT   += --destination-ranges=$(MYIP)/32
 PARAM  = --network="default" --priority=900
 RULES  = --rules=tcp:22,udp:60000-61000
@@ -18,15 +17,15 @@ RULES  = --rules=tcp:22,udp:60000-61000
 firewall: fw-list fw-create 
 
 fw-create:
-	@$(CREATE) ssh-allow-in  $(ALLOW) $(IN)  $(RULES) $(PARAM)
-	@$(CREATE) ssh-allow-out $(ALLOW) $(OUT) $(RULES) $(PARAM)
+	@$(CREATE) ssh-in  $(ALLOW) $(IN)  $(RULES) $(PARAM)
+	@$(CREATE) ssh-out $(ALLOW) $(OUT) $(RULES) $(PARAM)
 
 fw-describe:
-	@$(DESCRIBE) ssh-allow-in
-	@$(DESCRIBE) ssh-allow-out
+	@$(DESCRIBE) ssh-in
+	@$(DESCRIBE) ssh-out
 
 fw-list:
 	@$(LIST)
 
-
 fw-delete:
+
