8aed44429d2b08bb7d72a57f126ac392794c4035
Hector Lecuanda <hector@lecuanda.com>
2018-09-11 12:56:12 -0700

    rypo on bootatrap

--- zbs.sh
+++ zbs.sh
@@ -1,8 +1,8 @@
 pkg install -y git
 pkg install -y zsh
-git clone git@guthub.com:hlecuanda/zconf.git .zconf
+git clone git@github.com:hlecuanda/zconf.git .zconf
 cd .zconf
 git checkout nokia3
-git clone --recursive git@guthub.com:sorin-ionescu/prezto.git .zprezto
+git clone --recursive git@github.com:sorin-ionescu/prezto.git .zprezto
 zconf
 
