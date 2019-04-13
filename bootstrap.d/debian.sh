6708c424b7c09065b0dcc2275942951fa89a9874
Hector <hector@lecuanda.com>
2019-04-13 12:40:33 +0000

    git.d installs

--- bootstrap.d/debian.sh
+++ bootstrap.d/debian.sh
@@ -1,11 +1,6 @@
 sudo apt install -y git tig zsh tmux jq figlet mosh pwgen vim-nox make
-echo 'gitlab.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBFSMqzJeV9rUzU4kWitGjeR4PWSa29SPqJ1fVkhtj3Hw9xjLVXVYrU9QlYWrOLXBpQ6KWjbjTDTdDkoohFzgbEY=' >> ~/.ssh/known_hosts
-ssh-keygen -H
-git clone git@gitlab.com:hlecuanda/zconf.git .zconf
-git clone --recursive git@github.com:sorin-ionescu/prezto.git .zconf/.zprezto
-make -C .zconf/zsh.d install
-make -C .zconf/vim.d install
-make -C .zconf/tmux.d install
-make -C .zconf/bin.d install
+git clone https://gitlab.com/hlecuanda/zconf.git .zconf
+git clone --recursive https://github.com/sorin-ionescu/prezto.git .zconf/.zprezto
+make -C .zconf install
 exec zsh -l
 
