27ca20bb6b1306f06b7d91be75c5f2eccc881967
Hector <hector@lecuanda.com>
2019-04-13 21:53:27 -0700

    updated bootstrap for debian

--- bootstrap.d/debian.sh
+++ bootstrap.d/debian.sh
@@ -1,6 +1,13 @@
-sudo apt install -y git tig zsh tmux jq figlet mosh pwgen vim-nox make
-git clone https://gitlab.com/hlecuanda/zconf.git .zconf
+sudo apt -q install -y curl diffuse figlet git googler jq make mosh pv pwgen rofi \
+thefuck tigtigervnc-standalone-server tigervnc-viewer tmux vim-nox vlc xdotool zsh
+xclip xterm feh exiftool highlight ranger
+clone https://gitlab.com/hlecuanda/zconf.git .zconf
 git clone --recursive https://github.com/sorin-ionescu/prezto.git .zconf/.zprezto
 make -C .zconf install
 exec zsh -l
+
+# binutils command-not-found coreutils curl diffutils figlet file git golang grep make man
+# mosh openssh procps pwgen python termux-am termux-api termux-exec termux-tools tig tmux
+# vim zsh git
+#  vim: set ft=zsh sw=4 tw=0 fdm=manual et :
 
