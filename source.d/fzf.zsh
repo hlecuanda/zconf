3a2bf4185a7266a96aca9be7130c53b06f21f351
Hector <hector@lecuanda.com>
2020-01-22 19:55:23 -0800

    fzf configured!

--- source.d/fzf.zsh
+++ source.d/fzf.zsh
@@ -1,13 +1,33 @@
-# Setup fzf
-# ---------
+#     ____      ____
+#    / __/___  / __/
+#   / /_/_  / / /_
+#  / __/ / /_/ __/
+# /_/   /___/_/-unufied setup
+#
 if [[ ! "$PATH" == */home/hector/.vim/bundle/fzf/bin* ]]; then
   export PATH="${PATH:+${PATH}:}/home/hector/.vim/bundle/fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/home/hector/.vim/bundle/fzf/shell/completion.zsh" 2> /dev/null
+if [[ true ]]  {
+# if (( $+commands[fzf] )) {
 
-# Key bindings
-# ------------
-source "/home/hector/.vim/bundle/fzf/shell/key-bindings.zsh"
+  # Main options
+  # typeset -x FZF_ALT_C_COMMAND=
+  # typeset -x FZF_ALT_C_OPTS=
+  # typeset -x FZF_COMPLETION_OPTS
+  # typeset -x FZF_COMPLETION_TRIGGER
+  # typeset -x FZF_CTRL_R_OPTS=
+  typeset -x FZF_CTRL_T_COMMAND=
+  typeset -x FZF_CTRL_T_OPTS="--preview='scope {} || tree -C {}' --preview-window=right:45% --ansi "
+  typeset -x FZF_TMUX=1
+  typeset -x FZF_DEFAULT_OPTS="-e --preview='scope {} || tree -C {}' --preview-window=right:45% --ansi --inline-info --border --layout reverse --height 40%"
+  typeset -x FZF_TMUX_HEIGHT='60%'
+
+  # Auto-completion
+  [[ $- == *i* ]] && source "/home/hector/.vim/bundle/fzf/shell/completion.zsh" 2> /dev/null
+
+  # Key bindings
+  source "/home/hector/.vim/bundle/fzf/shell/key-bindings.zsh"
+}
+#  d(-_-;)bm  hlo.mx 1579750309
+#  vim:  wrap et :
